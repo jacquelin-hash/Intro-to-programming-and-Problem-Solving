ca0e96441d11de456de69e427f5ee26e4dc948d1
Jacqueline Guimac <71201628+jacquelin-hash@users.noreply.github.com>
2024-08-21 14:02:54 -0400

    Add files via upload

---
+++ hw2_q3.py
@@ -0,0 +1,14 @@
+# Author: <Jacqueline Guimac>
+# Assignment / Part: HW2 - Q3
+# Date due: 2023-06-05
+# I pledge that I have completed this assignment without
+# collaborating with anyone else, in conformance with the
+# NYU School of Engineering Policies and Procedures on
+# Academic Misconduct.
+
+my_list = [53, 17, 39, 22, 81, 69]
+print(my_list[3])
+
+list1 = [2, 9, 5, 7]
+list2 = [1, 4, 2, 3]
+print(list1 + list2)
