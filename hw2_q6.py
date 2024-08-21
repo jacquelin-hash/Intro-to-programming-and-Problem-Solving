ca0df2d109d3b2fc506ac258577416e7422ca030
Jacqueline Guimac <71201628+jacquelin-hash@users.noreply.github.com>
2024-08-21 14:04:02 -0400

    Add files via upload

---
+++ hw2_q6.py
@@ -0,0 +1,16 @@
+# Author: <Jacqueline Guimac>
+# Assignment / Part: HW2 - Q6 (etc.)
+# Date due: 2023-06-05
+# I pledge that I have completed this assignment without
+# collaborating with anyone else, in conformance with the
+# NYU School of Engineering Policies and Procedures on
+# Academic Misconduct.
+
+print("Welcome to the College Admissions Application Tracker!")
+names = ""
+statuses = ""
+
+while name != "quit":
+      name = input("Enter the name of the applicant (or 'quit' to exit): ")
+      name = name.strip().capitalize()  
+
