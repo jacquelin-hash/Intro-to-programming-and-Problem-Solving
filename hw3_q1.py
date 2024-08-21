9838ede22cfc2a7c50c58d8492c04e108f631be9
Jacqueline Guimac <71201628+jacquelin-hash@users.noreply.github.com>
2024-08-21 14:04:25 -0400

    Add files via upload

---
+++ hw3_q1.py
@@ -0,0 +1,12 @@
+# Author: <Jacqueline Guimac >
+# Assignment / Part: HW3 - Q1 (etc.)
+# Date due: 2023-06-12
+# I pledge that I have completed this assignment without
+# collaborating with anyone else, in conformance with the
+# NYU School of Engineering Policies and Procedures on
+# Academic Misconduct. 
+
+def yards_to_miles(yards):
+    miles = yards / 1760.0
+    return miles
+
