33e7ed1404dedefb42b04f572b49bae1f74a1694
Jacqueline Guimac <71201628+jacquelin-hash@users.noreply.github.com>
2024-08-21 14:05:09 -0400

    Add files via upload

---
+++ hw3_q4.py
@@ -0,0 +1,13 @@
+# Author: <Jacqueline Guimac >
+# Assignment / Part: HW3 - Q4 (etc.)
+# Date due: 2023-06-12
+# I pledge that I have completed this assignment without
+# collaborating with anyone else, in conformance with the
+# NYU School of Engineering Policies and Procedures on
+# Academic Misconduct. 
+
+def my_shuffle(lst):
+    shuffled = lst[:]
+    n = len(shuffled)
+
+    
