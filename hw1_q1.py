83028ab1f5bc990e439614352304bc4171c03367
Jacqueline Guimac <71201628+jacquelin-hash@users.noreply.github.com>
2024-08-21 14:01:02 -0400

    Add files via upload

---
+++ hw1_q1.py
@@ -0,0 +1,13 @@
+"""
+Author: [Jacqueline Guimac]
+Assignment / Part: HW1 - Q1 (Hello, You!)
+Date due: 2023-02-09, 11:59pm
+I pledge that I have completed this assignment without
+collaborating with anyone else, in conformance with the
+NYU School of Engineering Policies and Procedures on
+Academic Misconduct.
+"""
+
+users_name = input("Please enter your name: ")
+users_age = input("Please enter your age: ")
+print(users_name + ", " + users_age + ", is taking CS-UY 1114.")
